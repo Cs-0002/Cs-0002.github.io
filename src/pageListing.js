7082d1698dfc7a048f93c50fae6d5682371e81bc
Cs-0002 <himegogo55@outlook.jp>
2026-09-20 19:01:33 +0900

    notesRender.jsとgalleryRender.jsをpageListing.jsとして統合。ついでにindex.htmlにgalleryを表示するように変更。

---
+++ src/pageListing.js
@@ -0,0 +1,56 @@
+function renderMeta(note) {
+  return `
+  ${note.tags.map(tag => `<span class="tag">${tag}</span>`).join('')}
+    <span class="date">${note.date}</span>
+    `;
+}
+
+function getNoteUrl(note, type) {
+  if (type === "notes") return `notes/${note.id}/`;
+  if (type === "gallery") return `gallery/${note.id}/`;
+}
+
+function renderCard(note, type) {
+  const url = getNoteUrl(note, type);
+  const card = document.createElement('div');
+  card.className = 'card';
+  card.dataset.tags = note.tags.join(',');
+  card.innerHTML = `
+  ${note.image ? `
+      <div style="width: 100%; aspect-ratio: 1; background-color: hsl(0 0% 12.5%); margin-bottom: 0.8rem;" >
+      <img src="${note.image}" style="width: 100%; height: 100%; object-fit: cover;">
+      </div>
+      ` : ''}
+    <h3><a href="${url}">${note.title}</a></h3>
+    ${renderMeta(note)}
+  `;
+  return card;
+}
+
+function renderList(data, type) {
+  const grid = document.querySelector('.card-grid');
+  if (grid) {
+    data.forEach(note => {
+      grid.appendChild(renderCard(note, type));
+    });
+  }
+}
+
+function renderListItem(note, type) {
+  const url = getNoteUrl(note, type);
+  const li = document.createElement('li');
+  li.innerHTML = `
+    <a href="${url}">${note.title}</a> - ${note.description}
+  `;
+  return li;
+}
+
+//notesのみに影響
+const metaEl = document.querySelector('.meta');
+if (metaEl) {
+  const filename = window.location.pathname.split('/').pop();
+  const id = filename.replace('.html', '');
+  const current = notesData.find(n => n.id === id);
+  metaEl.innerHTML = renderMeta(current)
+}
+
